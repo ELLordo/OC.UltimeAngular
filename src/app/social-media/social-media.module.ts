18764cd7b5a74c83873ecebbec9fc35696059f6e
ElLordo <alexis.deman1@gmail.com>
2023-06-07 23:15:09 +0200

    resolvez les donnees

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
 import { PostsService } from './services/post.service';
+import { PostResolver } from './resolvers/posts.resolver';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostListComponent
+  ],
   imports: [
     CommonModule,
     SocialMediaRoutingModule
   ],
   providers: [
-    PostsService
+    PostsService,
+    PostResolver
   ]
 })
 export class SocialMediaModule { }
